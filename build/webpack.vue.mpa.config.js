cbbecbb9db5b9da60d640db33977008aba1631b1
zhuotuo <zhuotuo@leagsoft.com>
2023-11-28 14:55:46 +0800

    feat: Vue MPA 改造

---
+++ build/webpack.vue.mpa.config.js
@@ -0,0 +1,108 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const UglifyJSWebpackPlugin = require('uglifyjs-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin-webpack5');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    home: path.resolve(__dirname, '../src/mpa/home.js'),
+    login: path.resolve(__dirname, '../src/mpa/login.js'),
+  },
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCSSExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024,
+          },
+        },
+        generator: {
+          filename: 'images/[name].[hash:6][ext]',
+        },
+      },
+      {
+        test: /\.ejs$/,
+        loader: 'ejs-loader',
+        options: {
+          esModule: false,
+        },
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'home.html',
+      template: path.resolve(__dirname, '../public/index.html'),
+      chunks: ['home'],
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'login.html',
+      template: path.resolve(__dirname, '../public/index.html'),
+      chunks: ['login'],
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../src/img'),
+          to: path.resolve(__dirname, '../dist/img'),
+        },
+      ],
+    }),
+    new MiniCSSExtractPlugin(),
+    new CleanWebpackPlugin(),
+    new VueLoaderPlugin(),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new UglifyJSWebpackPlugin(), new CssMinimizerPlugin()],
+    splitChunks: {
+      minSize: 30 * 1024,
+      chunks: 'all',
+      name: 'common',
+      cacheGroups: {
+        jquery: {
+          name: 'jquery',
+          test: /jquery\.js/,
+          chunks: 'all',
+        },
+        'lodash-es': {
+          name: 'lodash-es',
+          test: /lodash-es/,
+          chunks: 'all',
+        },
+      },
+    },
+  },
+};
--- src/Home.vue
+++ src/Home.vue
@@ -9,7 +9,7 @@
           </h1>
           <div class="fr clearfix" id="top1">
             <p class="fl">
-              <a @click="jumpToLogin" id="login">登录</a>
+              <a href="login.html" id="login">登录</a>
               <a href="#" id="reg">注册</a>
             </p>
             <form action="#" method="get" class="fl">
