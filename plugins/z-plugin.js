2d651c9fa5508ae759c81f8cbab718cdaa9f6c34
shuibuzhuo <2301140231@qq.com>
2023-12-21 11:13:25 +0800

    feat: shuibuzhuo-build 脚手架移植

---
+++ plugins/z-plugin.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = function(api, params) {
+  const dir = process.cwd()
+  const config = api.getWebpackConfig()
+
+  config.entry('login').add(path.resolve(dir, './src/login.js'))
+
+  
+  config.module
+    .rule("ejs")
+    .test(/\.ejs$/)
+    .exclude.add(/node_modules/)
+    .end()
+    .use("ejs-loader")
+    .loader("ejs-loader")
+    .options({
+      esModule: false,
+    });
+
+  const defaultHtmlWebpackPlugin = config.plugin('HtmlWebpackPlugin')
+  defaultHtmlWebpackPlugin.use(HtmlWebpackPlugin, [{
+    filename: 'index.html',
+    template: path.resolve(dir, './src/index.html'),
+    chunks: ['index']
+  }])
+
+  config.plugin("HtmlWebpackPlugin2").use(HtmlWebpackPlugin, [
+    {
+      filename: "login.html",
+      template: path.resolve(dir, "./src/login.html"),
+      chunks: ["login"],
+    },
+  ]);
+
+  config.plugin("ProvidePlugin").use(webpack.DefinePlugin, [
+    {
+      $: "jquery",
+      jQuery: "jquery",
+    },
+  ]);
+
+  config.plugin("CopyPlugin").use(CopyWebpackPlugin, [
+    {
+      patterns: [
+        {
+          from: path.resolve(dir, "./src/img"),
+          to: path.resolve(dir, "./dist/img"),
+        },
+      ],
+    },
+  ]);
+}
