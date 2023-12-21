271b8b06e37a0548ac7eaefc6edf8df4c1bb75ee
shuibuzhuo <2301140231@qq.com>
2023-12-21 14:24:07 +0800

    feat: 通用 vue 插件开发

--- plugins/z-plugin.vue.js
+++ plugins/z-plugin.vue.js
@@ -1,14 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = function (api, params) {
-  console.log('vue plugin run...');
   const dir = process.cwd();
   const config = api.getWebpackConfig();
-
-  config.entry('index').clear().add(path.resolve(dir, './src/main.js'));
 
   config.module
     .rule('ejs')
@@ -20,14 +16,6 @@ module.exports = function (api, params) {
     .options({
       esModule: false,
     });
-
-  config.module
-    .rule('vue')
-    .test(/\.vue$/)
-    .exclude.add(/node_modules/)
-    .end()
-    .use('vue-loader')
-    .loader('vue-loader');
 
   config.plugin('ProvidePlugin').use(webpack.ProvidePlugin, [
     {
@@ -46,6 +34,4 @@ module.exports = function (api, params) {
       ],
     },
   ]);
-
-  config.plugin('VueLoaderPlugin').use(VueLoaderPlugin);
 };
